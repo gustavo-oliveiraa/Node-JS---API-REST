171a95dedca67999b767663bde083c5c41f12a10
Gustavo Maxwel <maxwelgustavo@gmail.com>
2023-12-06 17:50:00 -0300

    Aula 03 criando novas rotas

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ import express from 'express'
 const app = express()
 
 // criar rota padrão ou raiz
-app.get('/', (req, res) => {
-    res.send('Curso de Node JS')
+app.get('/', (request, response) => {
+    response.send('Curso de Node JS')
+})
+
+app.get('/selecoes', (request, response) => {
+    response.send('Lista de seleções')
 })
 
 export default app
