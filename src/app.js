c4e670d8512f711765682bc892d0155cc49c9943
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-12-18 15:51:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
+
+// mock  ==> termo para mockar dados, estrutura basica de dados
+const selecoes = [ // array de objetos
+    { id: 1, selecao: 'Brasil', grupo: 'G' },
+    { id: 2, selecao: 'Suíça', grupo: 'G' },
+    { id: 3, selecao: 'Sérvia', grupo: 'G' },
+    { id: 4, selecao: 'Camarões', grupo: 'G' },
+]
 
 // criar rota padrão ou raiz
 app.get('/', (request, response) => {
@@ -8,7 +16,12 @@ app.get('/', (request, response) => {
 })
 
 app.get('/selecoes', (request, response) => {
-    response.send('Lista de seleções')
+    response.status(200).send(selecoes)
+})
+
+app.post('/selecoesEnvio', (request, response) => {
+    selecoes.push(request.body)
+    response.status(201).send('Seleção cadastrada com sucesso!')
 })
 
 export default app
