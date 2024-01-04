d7ba50beae52a60d213ef57158b599c72a485b0d
Gustavo <maxwelgustavo@gmail.com>
2024-01-04 15:50:15 -0300

    Refatoramos para o MVC Pattern Repository

--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -1,66 +1,37 @@
-import conexao from '../database/conexao.js'
+import SelecaoRepository from '../repositories/SelecaoRepository.js'
 
 class SelecaoController {
 
-  index(request, response) {
-    const sql = "SELECT * FROM selecoes;"
-    conexao.query(sql, (error, result) => {
-      if (error) {
-        response.status(404).json({ 'error': error })
-      } else {
-        response.status(200).json(result)
-      }
-    })
+  // Depois passar os codigos de status e mensagens indicando erros, no response
+
+  async index(request, response) {
+    const row = await SelecaoRepository.findAll()
+    response.json(row)
   }
 
-  show(request, response) {
+  async show(request, response) {
     const id = request.params.id
-    const sql = "SELECT * FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (error, result) => {
-      const linha = result[0]
-      if (error) {
-        response.status(404).json({ 'error': error })
-      } else {
-        response.status(200).json(linha)
-      }
-    })
+    const row = await SelecaoRepository.findById(id)
+    response.json(row)
   }
 
-  store(request, response) {
+  async store(request, response) {
     const selecao = request.body
-    const sql = "INSERT INTO selecoes SET ?;"
-    conexao.query(sql, selecao, (error, result) => {
-      if (error) {
-        response.status(404).json({ 'error': error })
-      } else {
-        response.status(201).json(result)
-      }
-    })
+    const row = await SelecaoRepository.create(selecao)
+    response.json(row)
   }
 
-  update(request, response) {
-    const id = request.params.id
+  async update(request, response) {
     const selecao = request.body
-    const sql = "UPDATE selecoes SET ? WHERE id=?;"
-    conexao.query(sql, [selecao, id], (error, result) => {
-      if (error) {
-        response.status(404).json({ 'error': error })
-      } else {
-        response.status(200).json(result)
-      }
-    })
+    const id = request.params.id
+    const row = await SelecaoRepository.update(selecao, id)
+    response.json(row)
   }
 
-  delete(request, response) {
+  async delete(request, response) {
     const id = request.params.id
-    const sql = "DELETE FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (error, result) => {
-      if (error) {
-        response.status(404).json({ 'error': error })
-      } else {
-        response.status(200).json(result)
-      }
-    })
+    const row = await SelecaoRepository.delete(id)
+    response.json(row)
   }
 }
 
