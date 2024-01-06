fa3c21c882c449fded4637c0c5d6ba741448b4f3
Gustavo <maxwelgustavo@gmail.com>
2024-01-05 21:06:11 -0300

    Projeto terminado e funcional, no futuro desenvolver a parte do front-end e aumentar mais as funcionalidades.

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,3 +1,5 @@
+// Orrrganizar esse arquivo em uma class
+
 import mysql from 'mysql'
 
 const conexao = mysql.createConnection({
@@ -9,5 +11,22 @@ const conexao = mysql.createConnection({
 })
 
 conexao.connect()
+
+/**
+ * Executa um código sql com ou sem valores
+ * @param {string} sql instrução sql a ser executada
+ * @param {string=id | [selecao, id]} valores a serem passados para o sql
+ * @param {string} mensagemReject mensagem a ser exibida
+ * @returns objeto da Promisse
+ */
+export const consulta = (sql, valores = '', mensagemReject) => {
+  return new Promise((resolve, reject) => {
+    conexao.query(sql, valores, (error, result) => {
+      if (error) return reject(mensagemReject)
+      const row = JSON.parse(JSON.stringify(result))
+      return resolve(row)
+    })
+  })
+}
 
 export default conexao
